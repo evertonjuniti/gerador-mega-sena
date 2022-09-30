2e7260406d7417f2889288453596b4f349dff1fc
Everton Juniti Ogura <everton@evertonogura.com>
2022-09-30 13:07:49 -0300

    Primeira versão do gerador

---
+++ GeradorWindowsForms/Form1.Designer.cs
@@ -0,0 +1,384 @@
+﻿namespace GeradorWindowsForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBox9 = new System.Windows.Forms.CheckBox();
+            this.checkBox10 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.checkBox1.Location = new System.Drawing.Point(80, 52);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(137, 29);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "15 números";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.checkBox2.Location = new System.Drawing.Point(80, 87);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(137, 29);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "14 números";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.checkBox3.Location = new System.Drawing.Point(80, 122);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(137, 29);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "13 números";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.checkBox4.Location = new System.Drawing.Point(80, 157);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(137, 29);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "12 números";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.checkBox5.Location = new System.Drawing.Point(80, 192);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(137, 29);
+            this.checkBox5.TabIndex = 4;
+            this.checkBox5.Text = "11 números";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.CheckedChanged += new System.EventHandler(this.checkBox5_CheckedChanged);
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.checkBox6.Location = new System.Drawing.Point(80, 227);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(137, 29);
+            this.checkBox6.TabIndex = 5;
+            this.checkBox6.Text = "10 números";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            this.checkBox6.CheckedChanged += new System.EventHandler(this.checkBox6_CheckedChanged);
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.checkBox7.Location = new System.Drawing.Point(80, 262);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(127, 29);
+            this.checkBox7.TabIndex = 6;
+            this.checkBox7.Text = "9 números";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            this.checkBox7.CheckedChanged += new System.EventHandler(this.checkBox7_CheckedChanged);
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.checkBox8.Location = new System.Drawing.Point(80, 297);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(127, 29);
+            this.checkBox8.TabIndex = 7;
+            this.checkBox8.Text = "8 números";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            this.checkBox8.CheckedChanged += new System.EventHandler(this.checkBox8_CheckedChanged);
+            // 
+            // checkBox9
+            // 
+            this.checkBox9.AutoSize = true;
+            this.checkBox9.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.checkBox9.Location = new System.Drawing.Point(80, 332);
+            this.checkBox9.Name = "checkBox9";
+            this.checkBox9.Size = new System.Drawing.Size(127, 29);
+            this.checkBox9.TabIndex = 8;
+            this.checkBox9.Text = "7 números";
+            this.checkBox9.UseVisualStyleBackColor = true;
+            this.checkBox9.CheckedChanged += new System.EventHandler(this.checkBox9_CheckedChanged);
+            // 
+            // checkBox10
+            // 
+            this.checkBox10.AutoSize = true;
+            this.checkBox10.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.checkBox10.Location = new System.Drawing.Point(80, 367);
+            this.checkBox10.Name = "checkBox10";
+            this.checkBox10.Size = new System.Drawing.Size(127, 29);
+            this.checkBox10.TabIndex = 9;
+            this.checkBox10.Text = "6 números";
+            this.checkBox10.UseVisualStyleBackColor = true;
+            this.checkBox10.CheckedChanged += new System.EventHandler(this.checkBox10_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 25);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Quais tipos quer fazer?:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(246, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 25);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Quantas apostas?:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(264, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 31);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(264, 85);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(125, 31);
+            this.textBox2.TabIndex = 13;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(264, 120);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(125, 31);
+            this.textBox3.TabIndex = 14;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(264, 155);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(125, 31);
+            this.textBox4.TabIndex = 15;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Enabled = false;
+            this.textBox5.Location = new System.Drawing.Point(264, 190);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(125, 31);
+            this.textBox5.TabIndex = 16;
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Enabled = false;
+            this.textBox6.Location = new System.Drawing.Point(264, 225);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(125, 31);
+            this.textBox6.TabIndex = 17;
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Enabled = false;
+            this.textBox7.Location = new System.Drawing.Point(264, 260);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(125, 31);
+            this.textBox7.TabIndex = 18;
+            this.textBox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Enabled = false;
+            this.textBox8.Location = new System.Drawing.Point(264, 295);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(125, 31);
+            this.textBox8.TabIndex = 19;
+            this.textBox8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Enabled = false;
+            this.textBox9.Location = new System.Drawing.Point(264, 330);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(125, 31);
+            this.textBox9.TabIndex = 20;
+            this.textBox9.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Enabled = false;
+            this.textBox10.Location = new System.Drawing.Point(264, 365);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(125, 31);
+            this.textBox10.TabIndex = 21;
+            this.textBox10.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(56, 418);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(276, 50);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "Gerar números da sorte!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(453, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(234, 25);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Número da sorte gerados:";
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(453, 50);
+            this.textBox11.Multiline = true;
+            this.textBox11.Name = "textBox11";
+            this.textBox11.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox11.Size = new System.Drawing.Size(537, 418);
+            this.textBox11.TabIndex = 24;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1002, 482);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox10);
+            this.Controls.Add(this.checkBox9);
+            this.Controls.Add(this.checkBox8);
+            this.Controls.Add(this.checkBox7);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gerador de números para jogos da Mega-Sena";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox3;
+        private CheckBox checkBox4;
+        private CheckBox checkBox5;
+        private CheckBox checkBox6;
+        private CheckBox checkBox7;
+        private CheckBox checkBox8;
+        private CheckBox checkBox9;
+        private CheckBox checkBox10;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private TextBox textBox9;
+        private TextBox textBox10;
+        private Button button1;
+        private Label label3;
+        private TextBox textBox11;
+    }
+}
---
+++ GeradorWindowsForms/Form1.cs
@@ -0,0 +1,215 @@
+namespace GeradorWindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Enabled = checkBox1.Checked;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox2.Enabled = checkBox2.Checked;
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox3.Enabled = checkBox3.Checked;
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox4.Enabled = checkBox4.Checked;
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox5.Enabled = checkBox5.Checked;
+        }
+
+        private void checkBox6_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox6.Enabled = checkBox6.Checked;
+        }
+
+        private void checkBox7_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox7.Enabled = checkBox7.Checked;
+        }
+
+        private void checkBox8_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox8.Enabled = checkBox8.Checked;
+        }
+
+        private void checkBox9_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox9.Enabled = checkBox9.Checked;
+        }
+
+        private void checkBox10_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox10.Enabled = checkBox10.Checked;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                validaCheckboxes();
+                validaApostas();
+                fazApostas();
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void validaCheckboxes()
+        {
+            if (!(checkBox1.Checked ||
+                  checkBox2.Checked ||
+                  checkBox3.Checked ||
+                  checkBox4.Checked ||
+                  checkBox5.Checked ||
+                  checkBox6.Checked ||
+                  checkBox7.Checked ||
+                  checkBox8.Checked ||
+                  checkBox9.Checked ||
+                  checkBox10.Checked))
+                throw new Exception("Nenhum tipo de aposta selecionado. Favor selecionar ao menos 1 tipo.");
+        }
+
+        private void validaApostas()
+        {
+            List<String> erros = new List<string>();
+
+            if (checkBox1.Checked)
+                if (!validaNumero(textBox1.Text))
+                    erros.Add("Número inválido para o tipo de 15 apostas;");
+
+            if (checkBox2.Checked)
+                if (!validaNumero(textBox2.Text))
+                    erros.Add("Número inválido para o tipo de 14 apostas;");
+
+            if (checkBox3.Checked)
+                if (!validaNumero(textBox3.Text))
+                    erros.Add("Número inválido para o tipo de 13 apostas;");
+
+            if (checkBox4.Checked)
+                if (!validaNumero(textBox4.Text))
+                    erros.Add("Número inválido para o tipo de 12 apostas;");
+
+            if (checkBox5.Checked)
+                if (!validaNumero(textBox5.Text))
+                    erros.Add("Número inválido para o tipo de 11 apostas;");
+
+            if (checkBox6.Checked)
+                if (!validaNumero(textBox6.Text))
+                    erros.Add("Número inválido para o tipo de 10 apostas;");
+
+            if (checkBox7.Checked)
+                if (!validaNumero(textBox7.Text))
+                    erros.Add("Número inválido para o tipo de 9 apostas;");
+
+            if (checkBox8.Checked)
+                if (!validaNumero(textBox8.Text))
+                    erros.Add("Número inválido para o tipo de 8 apostas;");
+
+            if (checkBox9.Checked)
+                if (!validaNumero(textBox9.Text))
+                    erros.Add("Número inválido para o tipo de 7 apostas;");
+
+            if (checkBox10.Checked)
+                if (!validaNumero(textBox10.Text))
+                    erros.Add("Número inválido para o tipo de 6 apostas;");
+
+            if (erros.Count > 0)
+                throw new Exception(String.Join(Environment.NewLine, erros.ToArray()));
+        }
+
+        private bool validaNumero(String numero)
+        {
+            int resultado;
+
+            return int.TryParse(numero, out resultado);
+        }
+
+        private void fazApostas()
+        {
+            List<String> apostas = new List<string>();
+
+            if (checkBox1.Checked)
+                gerarNumeros(textBox1.Text, 15, ref apostas);
+
+            if (checkBox2.Checked)
+                gerarNumeros(textBox2.Text, 14, ref apostas);
+
+            if (checkBox3.Checked)
+                gerarNumeros(textBox3.Text, 13, ref apostas);
+
+            if (checkBox4.Checked)
+                gerarNumeros(textBox4.Text, 12, ref apostas);
+
+            if (checkBox5.Checked)
+                gerarNumeros(textBox5.Text, 11, ref apostas);
+
+            if (checkBox6.Checked)
+                gerarNumeros(textBox6.Text, 10, ref apostas);
+
+            if (checkBox7.Checked)
+                gerarNumeros(textBox7.Text, 9, ref apostas);
+
+            if (checkBox8.Checked)
+                gerarNumeros(textBox8.Text, 8, ref apostas);
+
+            if (checkBox9.Checked)
+                gerarNumeros(textBox9.Text, 7, ref apostas);
+
+            if (checkBox10.Checked)
+                gerarNumeros(textBox10.Text, 6, ref apostas);
+
+            textBox11.Text = String.Join(Environment.NewLine, apostas.ToArray());
+        }
+
+        private void gerarNumeros(String quantidade, int numeros, ref List<String> apostas)
+        {
+            int quantidadeInt = int.Parse(quantidade);
+            Random random = new Random();
+
+            for (int indiceQuantidade = 1; indiceQuantidade <= quantidadeInt; indiceQuantidade++)
+            {
+                List<int> numerosGerados = new List<int>();
+
+                for (int indiceNumeros = 1; indiceNumeros <= numeros; indiceNumeros++)
+                {
+                    bool gerar = true;
+
+                    while(gerar)
+                    {
+                        int numeroGerado = random.Next(1, 61);
+
+                        if (!numerosGerados.Contains(numeroGerado))
+                        {
+                            numerosGerados.Add(numeroGerado);
+                            gerar = false;
+                        }
+                    }
+                    
+                }
+
+                String aposta = "";
+
+                foreach (int numeroGerado in numerosGerados)
+                    aposta += "-" + numeroGerado.ToString();
+
+                apostas.Add(aposta.Substring(1, aposta.Length - 1));
+            }
+        }
+    }
+}
